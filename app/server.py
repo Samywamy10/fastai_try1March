ae516bd5060d96486e230a9c6371cdc575f8a085
Sam Wright <sam.wright@readify.net>
2019-10-08 22:38:57 +1100

    readd missing stuff

--- app/server.py
+++ app/server.py
@@ -1,6 +1,8 @@
 import aiohttp
 import asyncio
 import uvicorn
+from fastai import *
+from fastai.vision import *
 from io import BytesIO
 from starlette.applications import Starlette
 from starlette.middleware.cors import CORSMiddleware
